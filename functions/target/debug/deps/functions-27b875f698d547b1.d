714e6f86056bfb8944f18a1fd08fb6d45c1266e5
Alexander Burkut <a.burkut90@gmail.com>
2025-11-22 16:45:45 +0300

    Add functions

---
+++ functions/target/debug/deps/functions-27b875f698d547b1.d
@@ -0,0 +1,5 @@
+/Users/alexanderburkut/workspace/rust-book-projects/functions/target/debug/deps/functions-27b875f698d547b1.d: src/main.rs
+
+/Users/alexanderburkut/workspace/rust-book-projects/functions/target/debug/deps/functions-27b875f698d547b1: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/alexanderburkut/workspace/rust-book-projects/functions/target/debug/functions: /Users/alexanderburkut/workspace/rust-book-projects/functions/src/main.rs
