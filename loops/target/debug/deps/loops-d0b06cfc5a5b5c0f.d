70b9843db944452931b7ea6be1534150781697e4
Alexander Burkut <a.burkut90@gmail.com>
2025-11-22 17:24:27 +0300

    Add loops

---
+++ loops/target/debug/deps/loops-d0b06cfc5a5b5c0f.d
@@ -0,0 +1,5 @@
+/Users/alexanderburkut/workspace/rust-book-projects/loops/target/debug/deps/loops-d0b06cfc5a5b5c0f.d: src/main.rs
+
+/Users/alexanderburkut/workspace/rust-book-projects/loops/target/debug/deps/loops-d0b06cfc5a5b5c0f: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/Users/alexanderburkut/workspace/rust-book-projects/loops/target/debug/loops: /Users/alexanderburkut/workspace/rust-book-projects/loops/src/main.rs
