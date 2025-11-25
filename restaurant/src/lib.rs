7244b81743d11614c27d298af249a18e8e31ef12
Alexander Burkut <a.burkut90@gmail.com>
2025-11-25 18:47:17 +0300

    Update restaurants

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -15,26 +15,42 @@
 // }
 
 
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+//
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+//
+// pub fn eat_at_restaurant() {
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+//
+//     // meal.seasonal_fruit = String::from("blueberries");
+// }
 
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
 }
 
-pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
 
-    // meal.seasonal_fruit = String::from("blueberries");
+mod customer {
+    use crate::front_of_house::hosting;
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
 }
