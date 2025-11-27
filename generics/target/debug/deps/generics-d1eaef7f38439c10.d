6f4172e89f6590e7487b473609a8cf4760e75b75
Alexander Burkut <a.burkut90@gmail.com>
2025-11-27 18:36:14 +0300

    Add generics

---
+++ generics/target/debug/deps/generics-d1eaef7f38439c10.d
@@ -0,0 +1,5 @@
+/Users/alexanderburkut/workspace/rust-book-projects/generics/target/debug/deps/generics-d1eaef7f38439c10.d: src/main.rs
+
+/Users/alexanderburkut/workspace/rust-book-projects/generics/target/debug/deps/generics-d1eaef7f38439c10: src/main.rs
+
+src/main.rs:
---
+++ generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+/Users/alexanderburkut/workspace/rust-book-projects/generics/target/debug/generics: /Users/alexanderburkut/workspace/rust-book-projects/generics/src/main.rs
