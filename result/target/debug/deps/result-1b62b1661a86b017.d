b94949396ea4a41404abb3e257751945390277df
Alexander Burkut <a.burkut90@gmail.com>
2025-11-27 17:46:41 +0300

    Add result

---
+++ result/target/debug/deps/result-1b62b1661a86b017.d
@@ -0,0 +1,5 @@
+/Users/alexanderburkut/workspace/rust-book-projects/result/target/debug/deps/result-1b62b1661a86b017.d: src/main.rs
+
+/Users/alexanderburkut/workspace/rust-book-projects/result/target/debug/deps/result-1b62b1661a86b017: src/main.rs
+
+src/main.rs:
---
+++ result/target/debug/result.d
@@ -0,0 +1,1 @@
+/Users/alexanderburkut/workspace/rust-book-projects/result/target/debug/result: /Users/alexanderburkut/workspace/rust-book-projects/result/src/main.rs
