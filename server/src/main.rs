f3f962b84e2cfbdda79bf2bf54b1b348726f6649
Alexander Burkut <a.burkut90@gmail.com>
2025-12-07 14:35:28 +0100

    Add server

---
+++ server/src/main.rs
@@ -0,0 +1,47 @@
+use server::ThreadPool;
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    println!("Server is started");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Server is shutting down");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, html_path) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        "GET /page HTTP/1.1" => ("HTTP/1.1 200 OK", "page.html"),
+        _ => ("HTTP/1.1 404 Not Found", "404.html"),
+    };
+
+    let contents = fs::read_to_string(html_path).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
